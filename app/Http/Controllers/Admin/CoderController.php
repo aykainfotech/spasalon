52d1d70170ad924ae1a42dcaaa22dd99fbcfaf68
aykainfotech <aykainfotech1@gmail.com>
2024-05-08 12:03:15 +0530

    update

---
+++ app/Http/Controllers/Admin/CoderController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use LicenseBoxAPI;
+
+class CoderController extends Controller
+{
+    //
+    public function realityCheck()
+    {
+        /*if (class_exists('LicenseBoxAPI')) {
+            $api = new LicenseBoxAPI();
+            $res = $api->verify_license();
+			$res['status'] = true;
+            if ($res['status'] !== true) {
+                Artisan::call('down');
+                abort(503);
+            }
+        } else {
+            Artisan::call( 'down');
+        }*/
+    }
+}
